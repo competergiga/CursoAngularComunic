98157afbada13233559a157d594d8129008479bb
ComPeter <competergiga@gmail.com>
2021-04-02 00:31:59 +0200

    Primer commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  nombre:string='Nombre del usuario';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  cambiarNombre(){
+    this.nombre = "Pedro";
+  }
+
+}
